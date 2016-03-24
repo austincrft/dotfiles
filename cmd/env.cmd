6a3c09b09395e828f9ea09d846423c9d647168c0
austincrft <austincrft@gmail.com>
2016-03-24 11:00:13 -0500

    cmd: yay for hacky bashrc clone in windows!

---
+++ cmd/env.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+:: Python3
+DOSKEY python3="C:\Python34\python.exe"
+DOSKEY pythonw3="C:\Python34\pythonw.exe"
+
+:: Aliases
+DOSKEY ls=ls --color
+DOSKEY la=ls -a --color
+DOSKEY ll=ls -l --color
