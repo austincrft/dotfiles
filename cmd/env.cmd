1e565c5619308dddd5c4337d8988c85d346605e3
Craft, Austin <austincrft@gmail.com>
2016-04-13 17:05:34 -0500

    vimrc and env.cmd changes

--- cmd/env.cmd
+++ cmd/env.cmd
@@ -1,10 +1,17 @@
 @echo off
 
-:: Python3
-DOSKEY python3="C:\Python34\python.exe"
-DOSKEY pythonw3="C:\Python34\pythonw.exe"
+:: Python2
+DOSKEY python2="C:\Python27\python.exe" $*
+DOSKEY pythonw2="C:\Python27\pythonw.exe" $*
 
-:: Aliases
+:: Python3
+DOSKEY python3="C:\Python35\python.exe" $*
+DOSKEY pythonw3="C:\Python35\pythonw.exe" $*
+
+:: ls
 DOSKEY ls=ls --color
 DOSKEY la=ls -a --color
 DOSKEY ll=ls -l --color
+
+:: cd
+DOSKEY cdu=cd %USERPROFILE%
