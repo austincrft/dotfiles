43689fc6d3ebcab5f8116921e652c7cbc7db08e9
Austin Craft <austincrft@gmail.com>
2017-03-14 11:52:35 -0500

    cmd: env.cmd cleanup

--- cmd/env.cmd
+++ cmd/env.cmd
@@ -4,31 +4,16 @@
 chcp 65001 > nul
 
 :: ls
-SET ls_options=-I "NTUSER.*" -I "ntuser.*" --color
+SET ls_options=-I "NTUSER.*" -I "ntuser.*" -I "Desktop.ini" --color
 DOSKEY ls=ls %ls_options% $*
 DOSKEY la=ls -a %ls_options% $*
 DOSKEY ll=ls -l %ls_options% $*
 
-:: grep
-DOSKEY grep=grep --color=auto $*
-
 :: find
-DOSKEY find="C:\tools\cygwin64\bin\find.exe" $*
-DOSKEY findf="C:\tools\cygwin64\bin\find.exe" $* -type f
-DOSKEY findd="C:\tools\cygwin64\bin\find.exe" $* -type d
+DOSKEY find="C:\Program Files\Git\usr\bin\find.exe" $*
 
-:: cd
-DOSKEY cdu=cd %USERPROFILE%
-
-:: .NET
-DOSKEY msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" $*
-
-:: pandoc
-DOSKEY pandoc="%USERPROFILE%\AppData\Local\Pandoc\pandoc.exe" $*
-
-DOSKEY pip="C:\Python36\Scripts\pip.exe" $*
-DOSKEY pip2="C:\Python27\Scripts\pip.exe" $*
+:: fzf
+DOSKEY fzf="C:\Users\acraft\Downloads\fzf-0.16.6-windows_amd64\fzf.exe" $*
 
 :: Work-Specific Aliases
-SET work_aliases="%USERPROFILE%\work\aliases.cmd"
-IF EXIST %work_aliases% CALL %work_aliases%
+CALL %USERPROFILE%\work\aliases.cmd 2> nul
