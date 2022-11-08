481edac97de98a91f51b768ef6b6ec793aa135a3
Austin Craft <austincrft@gmail.com>
2022-11-08 10:37:08 -0600

    pwsh: select-string aliases

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -10,6 +10,8 @@ Set-PSReadlineOption -BellStyle None
 # Aliases
 Set-Alias -Name g -Value git
 Set-Alias -Name unzip -Value Expand-Archive
+Set-Alias -Name ss -Value Select-String
+Set-Alias -Name grep -Value Select-String
 
 # Import Work-Specific Commands/Aliases
 $workAliasScript = "$env:USERPROFILE/work/PowerShell/profile.ps1"
