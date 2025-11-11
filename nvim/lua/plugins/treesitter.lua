9857adb2cfc1c6f550d63404f8fd5bd8c641ac67
Austin Craft <austincrft@gmail.com>
2025-11-10 19:05:37 -0600

    nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,11 +8,77 @@ return {
         highlight = {
           enable = true,
         },
+        indent = {
+          enable = true
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn", -- set to `false` to disable one of the mappings
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+        ensure_installed = {
+          "bicep",
+          "c_sharp",
+          "dockerfile",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "powershell",
+          "sql",
+          "typescript",
+          "xml",
+          "yaml",
+        },
       })
     end,
   },
   {
     "nvim-treesitter/playground",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function ()
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup({
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["as"] = { query = "@local.scope", query_group = "locals" },
+              ["is"] = { query = "@local.scope", query_group = "locals" },
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+              ["]s"] = { query = "@local.scope", query_group = "locals" },
+              ["]z"] = { query = "@fold", query_group = "folds" },
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+              ["[s"] = { query = "@local.scope", query_group = "locals" },
+              ["[z"] = { query = "@fold", query_group = "folds" },
+            },
+          },
+        },
+      })
+    end,
   },
 }
